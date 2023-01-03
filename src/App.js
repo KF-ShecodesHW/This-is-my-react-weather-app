f46e7e45b700d22d62c1a6d632d6735240e5d72e
kellzbellz45 <kelly@kellyfaria.com>
2023-01-02 19:50:55 -0500

    Added repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This is coded by Kelly Faria and is{" "}
+        <a
+          href="https://github.com/KF-ShecodesHW/This-is-my-react-weather-app"
+          target="_blank"
+        >
+          opened-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
