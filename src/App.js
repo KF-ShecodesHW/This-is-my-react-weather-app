1215b5b3b85a4e76b70cea2d088d10e3e9ff9d07
kellzbellz45 <kelly@kellyfaria.com>
2023-01-02 19:45:14 -0500

    Removed H2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      React Weather App
-      <h2>This is a react app I am working on</h2>
-    </div>
-  );
+  return <div className="App">React Weather App</div>;
 }
